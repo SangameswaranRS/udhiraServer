9378b52db4315b0cc04e4f81db061915190da459
LAPTOP-7QQ4KURV\Sangameswaran <blackdrafteaglekrish@gmail.com>
2017-11-18 23:16:40 +0530

    login and register API Complete

--- availableRoutes.js
+++ availableRoutes.js
@@ -3,12 +3,13 @@
     var app=express();
     var bodyParser=require('body-parser');
     var registrationController=require('./registrationController');
+    var loginController=require('./loginController');
     var Router=express.Router();
     Router.post('/register',function (req,res) {
         registrationController.registerUser(req,res);
     });
     Router.post('/login',function (req,res) {
-
+        loginController.onLoginInfoSubmit(req,res);
     });
     module.exports=Router;
 })();
--- loginController.js
+++ loginController.js
@@ -1,5 +1,75 @@
 (function () {
+    var under=require('underscore');
+    var loginDAO=require('./loginDAO');
+    var password;
+    var userEmailId;
+    var userImei;
     module.exports.onLoginInfoSubmit=function (req,res) {
-        
+        password=req.body.password;
+        userEmailId=req.body.userEmailId;
+        userImei=req.body.imeiIndex;
+        loginDAO.getUserRecord(req.body.userEmailId,function (err,data) {
+            if(err){
+                var errorResponse={
+                    statusCode : 341,
+                    message : err.message
+                } ;
+                res.status(341).send(errorResponse);
+                console.log(err);
+            }else {
+               var newList=under.map(data,function (dat) {
+                   return {
+                       name : dat.userName,
+                       pass : dat.password,
+                       imei : dat.imeiIndex
+                   }
+               });
+               var actualPassword=newList[0].pass;
+               var actualImei=newList[0].imei;
+               if(actualPassword === password){
+                   if(userImei===actualImei){
+                       var successResponse={
+                           statusCode : 200,
+                           message : "Logged in successfully"
+                       } ;
+                       res.send(successResponse);
+                   }else {
+                       console.log("Passwords match");
+                       if(!actualImei){
+                           console.log("new login inserting imei ");
+                           console.log(userEmailId);
+                           loginDAO.updateIMEI(userImei,userEmailId,function (err,data) {
+                              if(err){
+                                  console.log("login failed to insert IMEI");
+                                  res.send(err);
+                              } else {
+                                  var successResponse={
+                                      statusCode : 200,
+                                      message : "Logged in successfully"
+                                  } ;
+                                  res.send(successResponse);
+                              }
+
+                           });
+                       }else {
+                           var failureResponse={
+                               statusCode : 201,
+                               message : "login from multiple Devices blocked"
+                           };
+                           res.send(failureResponse);
+                       }
+                   }
+
+               }else {
+                   var failureResponse={
+                       statusCode : 202,
+                       message : "Passwords dont match"
+                   };
+                   res.status(202).send(failureResponse);
+               }
+
+
+            }
+        })
     }
 })();
